33f9dbf59ec371649c31f893c5279cb0c423d2e3
SPHLam <samphanhoailam@gmail.com>
2024-06-11 16:02:21 +0700

    Horizontal Movement
    - Implement movement for Player (left and right)
    - Implement Player sprite flipping according to direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,38 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidBody2D;
+    SpriteRenderer mySpriteRenderer;
+
+    [SerializeField] float moveSpeed = 5f;
+
     void Start()
     {
-        
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue inputValue)
     {
         moveInput = inputValue.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        if (moveInput.x > 0)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+        else if (moveInput.x < 0)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidBody2D.velocity.y);
+        myRigidBody2D.velocity = playerVelocity;
     }
 }
