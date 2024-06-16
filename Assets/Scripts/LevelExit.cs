e2b869402f4c6c7e5140aaa13f90e7fbabd12d32
SPHLam <samphanhoailam@gmail.com>
2024-06-16 23:38:29 +0700

    Implement Load Next Level functionality
    Fixing minor bug (level design)
    Note: if the player is at the last level, load back from first

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    float levelLoadDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // getting the scene build index
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) // reach the end
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
