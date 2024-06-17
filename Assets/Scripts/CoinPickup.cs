48800aa2fd7ccce1977f573a1212939e0cbb1b40
SPHLam <samphanhoailam@gmail.com>
2024-06-17 16:01:41 +0700

    Adding UI
    Score text and Lives text

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinAudioClip;
+    bool wasCollected = false; // prevent double collect 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !wasCollected)
         {
+            FindObjectOfType<GameSession>().AddPoint();
+            wasCollected = true;
             AudioSource.PlayClipAtPoint(coinAudioClip, Camera.main.transform.position);
             Destroy(gameObject);
         }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     int playerLives = 3;
+    int score = 0;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
     void Awake()
     {
         int numsOfGameSession = FindObjectsOfType<GameSession>().Length;
@@ -17,6 +22,12 @@ public class GameSession : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -36,11 +47,18 @@ public class GameSession : MonoBehaviour
         playerLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         StartCoroutine(LoadSceneDelay(currentSceneIndex));
+        livesText.text = playerLives.ToString();
     }
 
     void ResetGameSession()
     {
         StartCoroutine(RestartGameDelay());
+    }
+
+    void UpdatingUI()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
     }
 
     IEnumerator LoadSceneDelay(int sceneIndex)
@@ -52,7 +70,14 @@ public class GameSession : MonoBehaviour
     IEnumerator RestartGameDelay()
     {
         yield return new WaitForSecondsRealtime(1f);
+        score = 0;
         SceneManager.LoadScene(0);
         Destroy(gameObject);
+    }
+
+    public void AddPoint()
+    {
+        score += 100;
+        scoreText.text = score.ToString();
     }
 }
